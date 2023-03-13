1462cb799a0289274f8cfc82452c58cfbc126869
Karla <karlafernandezchirinos@gmail.com>
2023-03-13 10:52:43 +0100

    add list_quotes_filter-characters-and-quotes

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,38 @@ import { useState } from 'react';
 function App() {
   const[quotes, setQuotes] = useState(characters)
   const [filterQuotes, setFilterQuotes] = useState ('');
+  const [filterCharacters, setFilterCharacters] = useState('Personajes');
 
-  const handleQuote =()=>{
-
+  const handleQuote = (event)=> {
+      setFilterQuotes(event.target.value);
   }
-  const handleCharacter = () => {
+  
+  const handleCharacter = (event) => {
+      setFilterCharacters(event.target.value)
+  }
 
+
+  const renderListQuotes = () => {
+    return characters 
+      .filter ((eachQuote)=> {
+        return (eachQuote.quote.toLowerCase().includes(filterQuotes.toLowerCase()))
+      })
+      .filter ((eachCharacter) =>{
+        if (filterCharacters !== 'Personajes') {
+         return (eachCharacter.character.toLowerCase() === (filterCharacters.toLowerCase()))
+        } else {
+          return eachCharacter; 
+        }
+
+      })
+      .map ((eachQuote, index)=>{
+        return (
+          <li key={index} className='card'>
+            <p>{eachQuote.quote}</p>
+            <p>{eachQuote.character}</p>
+          </li>
+        )
+      })
   }
 
   return ( 
@@ -23,25 +49,30 @@ function App() {
             Firltar por frase
           <input 
           type= 'text'
-          id='forQuotes'
+          id='Quotes'
           onChange ={handleQuote}
+          value= {filterQuotes}
           >
           </input>
           </label>
           <label htmlFor='searchCharacters'>
             Firltar por personajes:
-          <input
-          className='select'
-          type='text'
-          id='forCharacters'
-          onChange={handleCharacter}
-          >
-          </input>
+          <select name='character' id='character' onChange={handleCharacter}>
+            <option value='Personajes'>Personajes </option>
+            <option value='Ross'> Ross </option>
+            <option value='Joey'> Joey </option>
+            <option value='Phoebe'> Phoebe</option>
+            <option value='Chandler'> Chandler</option>
+            <option value='Rachel'> Rachel </option>
+            <option value='Mónica'> Mónica </option>
+          </select>
           </label>
         </form>
       </header>
       <main>
-
+        <ul>
+          {renderListQuotes()}
+        </ul>
       </main>
   </div>
   );
